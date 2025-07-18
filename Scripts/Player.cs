df2babf5dc5a4323e142574e5cb28ff651f60528
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2025-07-19 00:04:13 +0200

    add: Today progress

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,8 +3,7 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-    [ExportGroup("Movement")]
-    [Export] private float max_speed;
+    [ExportGroup("Movement")] [Export] private float max_speed;
     [Export] private float acceleration;
     [Export] private float braking;
     [Export] private float air_acceleration;
@@ -12,10 +11,9 @@ public partial class Player : CharacterBody3D
     [Export] private float gravity_modifier;
     [Export] private bool isRunning;
 
-    [ExportGroup("Camera")]
-    [Export] private float look_sensivity;
+    [ExportGroup("Camera")] [Export] private float look_sensivity;
     [Export] private Vector2 camera_input;
-    
+
     [Export] private Camera3D camera;
     [Export] private Variant gravity = ProjectSettings.GetSetting("physics/3d/default_gravity");
 
@@ -26,11 +24,24 @@ public partial class Player : CharacterBody3D
 
     public override void _PhysicsProcess(double delta)
     {
-        base._PhysicsProcess(delta);
+        var move_input = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        Velocity = new Vector3(move_input.X, 0, move_input.Y) * max_speed;
+        MoveAndSlide();
+        
+        //Camera
+        RotateY(-camera_input.X * look_sensivity);
+        camera.RotateX(-camera_input.Y * look_sensivity);
+        camera_input.Y = 0;
+        
     }
 
     public override void _UnhandledInput(InputEvent @event)
     {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            camera_input = mouseMotion.Relative;
+        }
+
         base._UnhandledInput(@event);
     }
 }
